b7aecd914af2b3bced3488ab3cb54b484f4adaba
CISAC <carolisacosta99@gmail.com>
2022-09-12 16:15:02 +0200

    Initial commit

---
+++ Assets/CubeManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour
+{
+    public enum Mode
+    {
+        Start,
+        RandomStart,
+        InvokeRepeating,
+        Coroutine
+    }
+    public Mode playMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
